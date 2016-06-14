13aa634ea1a88a71fa0fa62e5716717ae9ccd028
m.motieian <mohammadmot@gmail.com>
2016-06-14 14:25:05 +0430

    update form

--- App_1/Form1.Designer.cs
+++ App_1/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 247);
+            this.ClientSize = new System.Drawing.Size(526, 252);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Read and Write Config file in XML format";
             this.ResumeLayout(false);
 
         }
