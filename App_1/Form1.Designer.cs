56495ef7a4887b4834e17ffd18e7f5f4fa1e1d53
m.motieian <mohammadmot@gmail.com>
2016-06-14 14:49:50 +0430

    update GUI

--- App_1/Form1.Designer.cs
+++ App_1/Form1.Designer.cs
@@ -28,21 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listViewUserInfo = new System.Windows.Forms.ListView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.columnHeaderId = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderUsername = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderPass = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderComment = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listViewUserInfo
+            // 
+            this.listViewUserInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewUserInfo.BackColor = System.Drawing.Color.Gainsboro;
+            this.listViewUserInfo.CheckBoxes = true;
+            this.listViewUserInfo.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderId,
+            this.columnHeaderUsername,
+            this.columnHeaderPass,
+            this.columnHeaderComment});
+            this.listViewUserInfo.GridLines = true;
+            this.listViewUserInfo.Location = new System.Drawing.Point(13, 13);
+            this.listViewUserInfo.MultiSelect = false;
+            this.listViewUserInfo.Name = "listViewUserInfo";
+            this.listViewUserInfo.Size = new System.Drawing.Size(494, 204);
+            this.listViewUserInfo.TabIndex = 0;
+            this.listViewUserInfo.UseCompatibleStateImageBehavior = false;
+            this.listViewUserInfo.View = System.Windows.Forms.View.Details;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Location = new System.Drawing.Point(432, 223);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // columnHeaderId
+            // 
+            this.columnHeaderId.Text = "Id";
+            // 
+            // columnHeaderUsername
+            // 
+            this.columnHeaderUsername.Text = "Username";
+            this.columnHeaderUsername.Width = 144;
+            // 
+            // columnHeaderPass
+            // 
+            this.columnHeaderPass.Text = "Password";
+            this.columnHeaderPass.Width = 121;
+            // 
+            // columnHeaderComment
+            // 
+            this.columnHeaderComment.Text = "Comment";
+            this.columnHeaderComment.Width = 162;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnAdd.Location = new System.Drawing.Point(510, 13);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(24, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnDelete.Location = new System.Drawing.Point(510, 42);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(24, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "-";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(526, 252);
+            this.ClientSize = new System.Drawing.Size(539, 258);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.listViewUserInfo);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "Form1";
             this.Text = "Read and Write Config file in XML format";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listViewUserInfo;
+        private System.Windows.Forms.ColumnHeader columnHeaderId;
+        private System.Windows.Forms.ColumnHeader columnHeaderUsername;
+        private System.Windows.Forms.ColumnHeader columnHeaderPass;
+        private System.Windows.Forms.ColumnHeader columnHeaderComment;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
 
--- App_1/Form1.cs
+++ App_1/Form1.cs
@@ -16,5 +16,35 @@ namespace App_1
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Allow the user to edit item text.
+            listViewUserInfo.LabelEdit = true;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int nRecordId = listViewUserInfo.Items.Count + 1;
+
+            // Create three items and three sets of sub-items for each item.
+            ListViewItem item = new ListViewItem(nRecordId.ToString(), 0);
+            item.Checked = true;
+            item.SubItems.Add("username");
+            item.SubItems.Add("password");
+            item.SubItems.Add("comment");
+
+            listViewUserInfo.Items.Add(item);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
